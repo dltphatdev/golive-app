b29bdda8b04395e8f2a2deac06aaecf42019dd8d
Phat Do <dltphat301@gmail.com>
2025-06-27 14:16:34 +0700

    update base url

--- constants/config.ts
+++ constants/config.ts
@@ -3,8 +3,8 @@ import Constants from "expo-constants";
 export const isRunningInBuild = Constants.appOwnership !== "expo";
 
 const CONFIG = {
-	SERVER_URL: "http://api-app.golive.com.vn/",
-	API_ENPOINT: "http://api-app.golive.com.vn/api/",
+	SERVER_URL: "https://api-app.golive.com.vn/",
+	API_ENPOINT: "https://api-app.golive.com.vn/api/",
 	ENPOINT_TIMEOUT: 18000,
 } as const;
 
